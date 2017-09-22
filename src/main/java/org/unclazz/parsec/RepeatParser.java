31ec4b778d4eed14693b8218d8c7de4a0ff990a4
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-22 23:19:36 +0900

    RepeatParserを追加

---
+++ src/main/java/org/unclazz/parsec/RepeatParser.java
@@ -0,0 +1,22 @@
+package org.unclazz.parsec;
+
+import java.io.IOException;
+
+public final class RepeatParser extends Parser {
+	private final SeqParser<String> _inner;
+	
+	RepeatParser(Parser original, int min, int max, int exactly, Parser sep) {
+		final RepeatConfig repConf = exactly == -1 
+				? RepeatConfig.range(min, max, sep)
+				: RepeatConfig.exactly(exactly, sep);
+		_inner = new SeqParser<String>(original.means(""), repConf);
+	}
+
+	@Override
+	protected ResultCore doParse(Context ctx) throws IOException {
+		return _inner.parse(ctx).detachValue();
+	}
+	public ValParser<Integer> count() {
+		return _inner.reReduce(() -> 0, (a, b) -> a + 1);
+	}
+}
