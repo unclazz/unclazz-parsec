53b07f6412075f3499534c3d5e0a9740313cd502
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-22 23:19:04 +0900

    Meansパーサーを追加

---
+++ src/main/java/org/unclazz/parsec/MeansValParser.java
@@ -0,0 +1,17 @@
+package org.unclazz.parsec;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+
+final class MeansValParser<T> extends ValParser<T> {
+	private final Parser _original;
+	private final Supplier<T> _supp;
+	public MeansValParser(Parser original, Supplier<T> supp) {
+		_original = original;
+		_supp = supp;
+	}
+	@Override
+	protected ValResultCore<T> doParse(Context ctx) throws IOException {
+		return _original.parse(ctx).attachValue(_supp.get());
+	}
+}
