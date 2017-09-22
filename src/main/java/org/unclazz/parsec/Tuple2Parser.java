25781a471e9a82a08f17b7267c4be81acbc22cb8
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-22 23:18:46 +0900

    Tuple2のためのパーサーを追加

---
+++ src/main/java/org/unclazz/parsec/Tuple2Parser.java
@@ -0,0 +1,28 @@
+package org.unclazz.parsec;
+
+import java.io.IOException;
+
+import org.unclazz.parsec.data.Tuple2;
+
+final class Tuple2Parser<T1, T2> extends ValParser<Tuple2<T1, T2>> {
+	private final ValParser<T1> _left;
+	private final ValParser<T2> _right;
+	
+	public Tuple2Parser(ValParser<T1> left, ValParser<T2> right) {
+		_left = left;
+		_right = right;
+	}
+
+	@Override
+	protected ValResultCore<Tuple2<T1, T2>> doParse(Context ctx) throws IOException {
+		final ValResult<T1> leftResult = _left.parse(ctx);
+		if (!leftResult.isSuccessful()) return failure(leftResult.message());
+		
+		final ValResult<T2> rightResult = _right.parse(ctx);
+		final boolean canBacktrack = leftResult.canBacktrack() && rightResult.canBacktrack();
+		
+		if (!rightResult.isSuccessful()) return failure(rightResult.message());
+		
+		return success(Tuple2.of(leftResult.value(), rightResult.value())).allowBacktrack(canBacktrack);
+	}
+}
