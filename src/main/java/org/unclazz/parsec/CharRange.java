161449740de6e1928d2335f86961399667004bcb
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-22 23:29:15 +0900

    CharRangeのjavadocを追加

--- src/main/java/org/unclazz/parsec/CharRange.java
+++ src/main/java/org/unclazz/parsec/CharRange.java
@@ -1,17 +1,26 @@
 package org.unclazz.parsec;
 
+/**
+ * 文字の範囲を表すクラスです。
+ */
 final class CharRange{
 	public final char start;
 	public final char end;
 	
-	public CharRange(char start, char end) {
+	CharRange(char start, char end) {
 		this.start = start < end ? start : end;
 		this.end = start < end ? end : start;
 	}
 	
+	/**
+	 * 文字がこの範囲に含まれるかどうかを判定します。
+	 * @param ch
+	 * @return
+	 */
 	public boolean contains(int ch) {
 		return start <= ch && ch <= end;
 	}
+	@Override
 	public String toString() {
 		if (start == end) return ParsecUtility.charToString(start);
 		return String.format("%s to %s", 
