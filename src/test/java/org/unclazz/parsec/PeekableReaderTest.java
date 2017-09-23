847180b7952e6a48316f12a199c61b9d659407ad
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-23 22:50:34 +0900

    PeekableReaderのテストケースを追加

---
+++ src/test/java/org/unclazz/parsec/PeekableReaderTest.java
@@ -0,0 +1,42 @@
+package org.unclazz.parsec;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.Test;
+
+public class PeekableReaderTest {
+
+	@Test
+	public void testPeek() throws IOException {
+		// Arrange
+		try (final PeekableReader r = new PeekableReader(new StringReader("012"))){
+			// Act
+			// Assert
+			assertThat(r.peek(), is('0' - 0));
+			r.read();
+			assertThat(r.peek(), is('1' - 0));
+			r.read();
+			assertThat(r.peek(), is('2' - 0));
+			r.read();
+			assertThat(r.peek(), is(-1));
+		}
+	}
+
+	@Test
+	public void testRead() throws IOException {
+		// Arrange
+		try (final PeekableReader r = new PeekableReader(new StringReader("012"))){
+			// Act
+			// Assert
+			assertThat(r.read(), is('0' - 0));
+			assertThat(r.read(), is('1' - 0));
+			assertThat(r.read(), is('2' - 0));
+			assertThat(r.read(), is(-1));
+		}
+	}
+
+}
