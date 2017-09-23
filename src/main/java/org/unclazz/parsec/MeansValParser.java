f0a9fc40191ec3743aed1839cf0cd6edad7409c0
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-23 22:18:15 +0900

    meansのテストケースを追加

--- src/main/java/org/unclazz/parsec/MeansValParser.java
+++ src/main/java/org/unclazz/parsec/MeansValParser.java
@@ -6,9 +6,11 @@ import java.util.function.Supplier;
 final class MeansValParser<T> extends ValParser<T> {
 	private final Parser _original;
 	private final Supplier<T> _supp;
-	public MeansValParser(Parser original, Supplier<T> supp) {
+	public MeansValParser(Parser original, Supplier<T> func) {
+		ParsecUtility.mustNotBeNull("original", original);
+		ParsecUtility.mustNotBeNull("func", func);
 		_original = original;
-		_supp = supp;
+		_supp = func;
 	}
 	@Override
 	protected ValResultCore<T> doParse(Context ctx) throws IOException {
---
+++ src/test/java/org/unclazz/parsec/MeansValParserTest.java
@@ -0,0 +1,23 @@
+package org.unclazz.parsec;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MeansValParserTest {
+
+	@Test
+	public void testParseString() {
+		final Parser abc = new KeywordParser("abc");
+		final ValParser<Integer> abcMeans123 = new KeywordParser("abc").means(123);
+		final ValParser<Integer> abcMeans456 = new KeywordParser("abc").means(() -> 456);
+		final ValResult<Integer> res1 = abcMeans123.parse("abc_");
+		final ValResult<Integer> res2 = abcMeans456.parse("abc_");
+		
+		assertTrue(res1.isSuccessful());
+		assertTrue(res1.value().equals(123));
+		assertTrue(res2.isSuccessful());
+		assertTrue(res2.value().equals(456));
+	}
+
+}
