ee0d03c47b376cb36c4d7df38cc2683d479385d8
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-24 20:51:03 +0900

    ExactCharCharClassの最適化コードを改善

--- src/main/java/org/unclazz/parsec/ExactCharCharClass.java
+++ src/main/java/org/unclazz/parsec/ExactCharCharClass.java
@@ -14,12 +14,27 @@ final class ExactCharCharClass extends CharClass{
 	}
 	@Override
 	public CharClass union(CharClass other) {
+		// レシーバが判定対象とする文字が先方クラスに含まれている場合　先方クラスをそのまま返す
 		if (other.contains(_ch)) return other;
+		
+		// 相手がCharRangeCharClassのインスタンスである場合
+		if (other instanceof CharRangeCharClass) {
+			// CharRangeCharClass.unionの方がより高度な最適化が行えるため処理を委譲
+			return ((CharRangeCharClass)other).union(this);
+		}
+		
+		// それ以外の場合　親クラスに処理を委譲
 		return super.union(other);
 	}
 	@Override
 	public CharClass plus(char ch) {
+		// 同じ文字の場合　自身をそのまま返す
 		if (_ch == ch) return this;
+		
+		// 隣接している場合　文字範囲に基づくクラスを生成して返す
+		if (_ch == ch - 1 || _ch == ch + 1) return between(_ch, ch);
+		
+		// それ以外の場合　親クラスに処理を委譲
 		return super.plus(ch);
 	}
 	@Override
--- src/test/java/org/unclazz/parsec/ExactCharCharClassTest.java
+++ src/test/java/org/unclazz/parsec/ExactCharCharClassTest.java
@@ -21,17 +21,45 @@ public class ExactCharCharClassTest {
 	@Test
 	public void testUnion() {
 		// Arrange
-		final CharClass c1 = new ExactCharCharClass('1');
-		final CharClass c2 = new ExactCharCharClass('2');
+		final CharClass clz_1 = new ExactCharCharClass('1');
+		final CharClass clz_2 = new ExactCharCharClass('2');
+		final CharClass clz_3 = new ExactCharCharClass('3');
 		
 		// Act
-		final CharClass c3 = c1.union(c2);
+		final CharClass clz_1_union_2 = clz_1.union(clz_2);
+		final CharClass clz_1_union_3 = clz_1.union(clz_3);
+		final CharClass clz_1_union_rng_1_3 = clz_1.union(CharClass.between('1', '3')); // 包含
+		final CharClass clz_1_union_rng_2_3 = clz_1.union(CharClass.between('2', '3')); // 隣接
+		final CharClass clz_1_union_rng_3_4 = clz_1.union(CharClass.between('3', '4')); // 独立
 		
 		// Assert
-		assertFalse(c3.contains('0'));
-		assertTrue(c3.contains('1'));
-		assertTrue(c3.contains('2'));
-		assertFalse(c3.contains('3'));
+		assertFalse(clz_1_union_2.contains('0'));
+		assertTrue(clz_1_union_2.contains('1'));
+		assertTrue(clz_1_union_2.contains('2'));
+		assertFalse(clz_1_union_2.contains('3'));
+		
+		assertFalse(clz_1_union_3.contains('0'));
+		assertTrue(clz_1_union_3.contains('1'));
+		assertFalse(clz_1_union_3.contains('2'));
+		assertTrue(clz_1_union_3.contains('3'));
+		
+		assertFalse(clz_1_union_rng_1_3.contains('0'));
+		assertTrue(clz_1_union_rng_1_3.contains('1'));
+		assertTrue(clz_1_union_rng_1_3.contains('2'));
+		assertTrue(clz_1_union_rng_1_3.contains('3'));
+		assertFalse(clz_1_union_rng_1_3.contains('4'));
+		
+		assertFalse(clz_1_union_rng_2_3.contains('0'));
+		assertTrue(clz_1_union_rng_2_3.contains('1'));
+		assertTrue(clz_1_union_rng_2_3.contains('2'));
+		assertTrue(clz_1_union_rng_2_3.contains('3'));
+		assertFalse(clz_1_union_rng_2_3.contains('4'));
+		
+		assertFalse(clz_1_union_rng_3_4.contains('0'));
+		assertTrue(clz_1_union_rng_3_4.contains('1'));
+		assertFalse(clz_1_union_rng_3_4.contains('2'));
+		assertTrue(clz_1_union_rng_3_4.contains('3'));
+		assertTrue(clz_1_union_rng_3_4.contains('4'));
 	}
 
 	@Test
@@ -41,12 +69,18 @@ public class ExactCharCharClassTest {
 		
 		// Act
 		final CharClass c2 = c1.plus('3');
+		final CharClass c3 = c1.plus('2');
 		
 		// Assert
 		assertFalse(c2.contains('0'));
 		assertTrue(c2.contains('1'));
 		assertFalse(c2.contains('2'));
 		assertTrue(c2.contains('3'));
+		
+		assertFalse(c3.contains('0'));
+		assertTrue(c3.contains('1'));
+		assertTrue(c3.contains('2'));
+		assertFalse(c3.contains('3'));
 	}
 
 }
