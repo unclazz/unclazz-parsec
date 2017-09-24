1e83cd46021b8c0f88326ecb3f31bafed10be4f4
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-24 21:57:39 +0900

    四則演算のサンプルを追加

---
+++ src/test/java/org/unclazz/parsec/sample/math/MathParserTest.java
@@ -0,0 +1,90 @@
+package org.unclazz.parsec.sample.math;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+import org.unclazz.parsec.Context;
+import org.unclazz.parsec.Mappers;
+import org.unclazz.parsec.ValParser;
+import org.unclazz.parsec.ValResult;
+import org.unclazz.parsec.ValResultCore;
+import org.unclazz.parsec.data.Tuple2;
+
+public final class MathParserTest {
+
+	@Test
+	public void testParse() {
+		final ValParser<Integer> math = new MathParser();
+		
+		final ValResult<Integer> result_2_add_3 = math.parse("2+3");
+		final ValResult<Integer> result_2_mul_3 = math.parse("2*3");
+		final ValResult<Integer> result_2_mul_3_add_1 = math.parse("2*3+1");
+		final ValResult<Integer> result_2_mul_ps_3_add_1_pe = math.parse("2*(3+1)");
+
+		result_2_add_3.ifSuccessful(a -> {
+			assertThat(a, is(5));
+		}, org.junit.Assert::fail);
+		
+		result_2_mul_3.ifSuccessful(a -> {
+			assertThat(a, is(6));
+		}, org.junit.Assert::fail);
+
+		result_2_mul_3_add_1.ifSuccessful(a -> {
+			assertThat(a, is(7));
+		}, org.junit.Assert::fail);
+		
+		result_2_mul_ps_3_add_1_pe.ifSuccessful(a -> {
+			assertThat(a, is(8));
+		}, org.junit.Assert::fail);
+	}
+
+}
+
+final class MathParser extends ValParser<Integer>{
+	private final ValParser<Integer> number = charBetween('0', '9').repMin(1).map(Mappers::digits);
+	
+	private ValParser<Integer> parens() {
+		return exact('(').cut().then(lazyVal(this::addSub)).then(exact(')'));
+	}
+	private ValParser<Integer> factor() {
+		return number.or(lazyVal(this::parens));
+	}
+	private ValParser<Integer> mulDiv() {
+		final ValParser<Integer> factor = lazyVal(this::factor);
+		return factor.then((charIn("*/").val().then(factor)).rep()).map(this::mulDivAddSub_mapper);
+	}
+	private ValParser<Integer> addSub() {
+		final ValParser<Integer> mulDiv = lazyVal(this::mulDiv);
+		return mulDiv.then((charIn("+-").val().then(mulDiv)).rep()).map(this::mulDivAddSub_mapper);
+	}
+	private ValParser<Integer> expr() {
+		return addSub().then(eof());
+	}
+	@Override
+	protected ValResultCore<Integer> doParse(Context ctx) throws IOException {
+		ctx = ctx.configure(a -> a.setLogAppender(System.out::println));
+		return expr().parse(ctx);
+	}
+	private Integer mulDivAddSub_mapper(Tuple2<Integer, List<Tuple2<String,Integer>>> expr) {
+		if (expr.item2().isEmpty()) return expr.item1();
+		int tmp = expr.item1();
+		for (final Tuple2<String,Integer> t : expr.item2()) {
+			if (t.item1().equals("*")) {
+				tmp *= t.item2();
+			} else if (t.item1().equals("/")) {
+				tmp /= t.item2();
+			} else if (t.item1().equals("+")) {
+				tmp += t.item2();
+			} else if (t.item1().equals("-")) {
+				tmp -= t.item2();
+			} else {
+				throw new IllegalArgumentException("unknown operator.");
+			}
+		}
+		return tmp;
+	}
+}
