30ec7b24090012566527f32bede02a23e3538fb8
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-24 12:15:08 +0900

    CharPositionのドキュメンテーションを追加

--- src/main/java/org/unclazz/parsec/CharPosition.java
+++ src/main/java/org/unclazz/parsec/CharPosition.java
@@ -1,7 +1,14 @@
 package org.unclazz.parsec;
 
+/**
+ * 文字位置を表すオブジェクトです。
+ */
 public final class CharPosition {
 	private static final CharPosition _bof = new CharPosition(1, 1, 0);
+	/**
+	 * BOF（データソースの先頭）を示すインスタンスを返します。
+	 * @return
+	 */
 	public static CharPosition ofBof() {
 		return _bof;
 	}
@@ -16,18 +23,38 @@ public final class CharPosition {
 		_index = index;
 	}
 	
+	/**
+	 * 行数（{@code 1}始まり）です。
+	 * @return
+	 */
 	public int line() {
 		return _line;
 	}
+	/**
+	 * 行の先頭からの位置（{@code 1}始まり）です。
+	 * @return
+	 */
 	public int column() {
 		return _column;
 	}
+	/**
+	 * データソースの先頭からのインデックス（{@code 0}始まり）です。
+	 * @return
+	 */
 	public int index() {
 		return _index;
 	}
+	/**
+	 * 行の先頭からの位置を{@code +1}した新しいインスタンスを返します。
+	 * @return
+	 */
 	public CharPosition nextColumn() {
 		return new CharPosition(_line, _column + 1, _index + 1);
 	}
+	/**
+	 * 行数を{@code +1}した新しいインスタンスを返します。
+	 * @return
+	 */
 	public CharPosition nextLine() {
 		return new CharPosition(_line + 1, 1, _index + 1);
 	}
