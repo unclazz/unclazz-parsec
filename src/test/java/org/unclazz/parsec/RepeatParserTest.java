0c7e28defd886c2656d9b48decfc7759e3b70457
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-25 23:07:55 +0900

    Parser.rep(...,sep)のテストケースを追加

--- src/test/java/org/unclazz/parsec/RepeatParserTest.java
+++ src/test/java/org/unclazz/parsec/RepeatParserTest.java
@@ -34,6 +34,29 @@ public class RepeatParserTest {
 	}
 
 	@Test
+	public void testParseTextReader_repParser() throws IOException {
+		// Arrange
+		final Parser p_a_rep = Parsers.exact('a').rep(Parsers.exact(','));
+		final Parser p_ab_rep = Parsers.charIn("ab").rep(Parsers.exact(','));
+		final Parser p_b_rep = Parsers.exact('b').rep(Parsers.exact(','));
+		
+		// Act
+		final Result r_a_rep = p_a_rep.parse("a,bbccc");
+		final Result r_ab_rep = p_ab_rep.parse("a,b,b,ccc");
+		final Result r_b_rep = p_b_rep.parse("a,b,b,ccc");
+		
+		// Assert
+		assertThat(r_a_rep.isSuccessful(), is(true));
+		assertThat(r_a_rep.end().index(), is(1));
+		
+		assertThat(r_ab_rep.isSuccessful(), is(true));
+		assertThat(r_ab_rep.end().index(), is(5));
+		
+		assertThat(r_b_rep.isSuccessful(), is(true));
+		assertThat(r_b_rep.end().index(), is(0));
+	}
+
+	@Test
 	public void testParseTextReader_repInt() throws IOException {
 		// Arrange
 		final Parser p_a_rep2 = Parsers.exact('a').rep(3);
@@ -81,6 +104,31 @@ public class RepeatParserTest {
 		
 		assertThat(r_a_rep23_vs_aaaacc.isSuccessful(), is(true));
 		assertThat(r_a_rep23_vs_aaaacc.end().index(), is(3));
+	}
+
+	@Test
+	public void testParseTextReader_repIntIntParser() throws IOException {
+		// Arrange
+		final Parser p_a_rep2 = Parsers.exact('a').rep(2, 3, Parsers.exact(','));
+		
+		// Act
+		final Result r_a_rep23_vs_abbccc = p_a_rep2.parse("a,b,b,ccc");
+		final Result r_a_rep23_vs_aabccc = p_a_rep2.parse("a,a,b,ccc");
+		final Result r_a_rep23_vs_aaaccc = p_a_rep2.parse("a,a,a,ccc");
+		final Result r_a_rep23_vs_aaaacc = p_a_rep2.parse("a,a,a,acc");
+		
+		// Assert
+		assertThat(r_a_rep23_vs_abbccc.isSuccessful(), is(false));
+		assertThat(r_a_rep23_vs_abbccc.end().index(), is(3));
+		
+		assertThat(r_a_rep23_vs_aabccc.isSuccessful(), is(true));
+		assertThat(r_a_rep23_vs_aabccc.end().index(), is(3));
+		
+		assertThat(r_a_rep23_vs_aaaccc.isSuccessful(), is(true));
+		assertThat(r_a_rep23_vs_aaaccc.end().index(), is(5));
+		
+		assertThat(r_a_rep23_vs_aaaacc.isSuccessful(), is(true));
+		assertThat(r_a_rep23_vs_aaaacc.end().index(), is(5));
 	}
 
 	@Test
