c619c94e85120baea613fae85dfaad945fe19c6c
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-25 09:07:03 +0900

    then系3クラスのソースコードを分割

--- src/main/java/org/unclazz/parsec/ThenTakeLeftParser.java
+++ src/main/java/org/unclazz/parsec/ThenTakeLeftParser.java
@@ -2,21 +2,19 @@ package org.unclazz.parsec;
 
 import java.io.IOException;
 
-final class ThenParser extends Parser {
-	private final Parser _left;
+final class ThenTakeLeftParser<T> extends ValParser<T> {
+	private final ValParser<T> _left;
 	private final Parser _right;
 	
-	protected ThenParser(Parser left, Parser right) {
-		ParsecUtility.mustNotBeNull("left", left);
-		ParsecUtility.mustNotBeNull("right", right);
+	protected ThenTakeLeftParser(ValParser<T> left, Parser right) {
 		_left = left;
 		_right = right;
 	}
 
 	@Override
-	protected ResultCore doParse(Context ctx) throws IOException {
+	protected ValResultCore<T> doParse(Context ctx) throws IOException {
         // 左側のパーサーでパース
-		final Result lres = _left.parse(ctx);
+		final ValResult<T> lres = _left.parse(ctx);
         // 結果NGの場合、ただちにその結果を呼び出し元に帰す
 		if (!lres.isSuccessful()) return lres;
 		
@@ -24,7 +22,8 @@ final class ThenParser extends Parser {
 		final Result rres = _right.parse(ctx);
         // バックトラック設定を合成
 		final boolean canBacktrack = lres.canBacktrack() && rres.canBacktrack();
-        // 右側の結果を、バックトラック設定のみカスタマイズし、呼び出し元に返す
-		return rres.allowBacktrack(canBacktrack);
+
+		return (rres.isSuccessful() ? success(lres.value()) : failure(rres.message()))
+				.allowBacktrack(canBacktrack);
 	}
 }
--- src/main/java/org/unclazz/parsec/ThenTakeRightParser.java
+++ src/main/java/org/unclazz/parsec/ThenTakeRightParser.java
@@ -2,29 +2,27 @@ package org.unclazz.parsec;
 
 import java.io.IOException;
 
-final class ThenParser extends Parser {
+final class ThenTakeRightParser<T> extends ValParser<T> {
 	private final Parser _left;
-	private final Parser _right;
+	private final ValParser<T> _right;
 	
-	protected ThenParser(Parser left, Parser right) {
-		ParsecUtility.mustNotBeNull("left", left);
-		ParsecUtility.mustNotBeNull("right", right);
+	protected ThenTakeRightParser(Parser left, ValParser<T> right) {
 		_left = left;
 		_right = right;
 	}
 
 	@Override
-	protected ResultCore doParse(Context ctx) throws IOException {
+	protected ValResultCore<T> doParse(Context ctx) throws IOException {
         // 左側のパーサーでパース
 		final Result lres = _left.parse(ctx);
         // 結果NGの場合、ただちにその結果を呼び出し元に帰す
-		if (!lres.isSuccessful()) return lres;
+		if (!lres.isSuccessful()) return failure(lres.message());
 		
         // 右側のパーサーでパース
-		final Result rres = _right.parse(ctx);
+		final ValResult<T> rres = _right.parse(ctx);
         // バックトラック設定を合成
 		final boolean canBacktrack = lres.canBacktrack() && rres.canBacktrack();
-        // 右側の結果を、バックトラック設定のみカスタマイズし、呼び出し元に返す
+
 		return rres.allowBacktrack(canBacktrack);
 	}
 }
