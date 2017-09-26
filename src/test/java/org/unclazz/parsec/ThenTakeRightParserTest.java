50cd6d247793b93c76225857b8b2031248d7f2ab
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-27 08:40:46 +0900

    then系のテストケースを追加

---
+++ src/test/java/org/unclazz/parsec/ThenTakeRightParserTest.java
@@ -0,0 +1,55 @@
+package org.unclazz.parsec;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class ThenTakeRightParserTest {
+
+	@Test
+	public void testParseString() {
+		final ValParser<String> p_0_1 = Parsers.exact('0').then(Parsers.exact('1').val());
+		final ValParser<String> p_0_cut_1 = Parsers.exact('0').cut().then(Parsers.exact('1').val());
+		
+		final ValResult<String> p_0_1_r_012 = p_0_1.parse("012");
+		final ValResult<String> p_0_cut_1_r_012 = p_0_cut_1.parse("012");
+		
+		assertThat(p_0_1_r_012.isSuccessful(), is(true));
+		assertThat(p_0_1_r_012.canBacktrack(), is(true));
+		assertThat(p_0_1_r_012.end().index(), is(2));
+		assertThat(p_0_1_r_012.value(), is("1"));
+		
+		assertThat(p_0_cut_1_r_012.isSuccessful(), is(true));
+		assertThat(p_0_cut_1_r_012.canBacktrack(), is(false));
+		assertThat(p_0_cut_1_r_012.end().index(), is(2));
+		assertThat(p_0_cut_1_r_012.value(), is("1"));
+		
+		final ValResult<String> p_0_1_r_X12 = p_0_1.parse("X12");
+		final ValResult<String> p_0_cut_1_r_X12 = p_0_cut_1.parse("X12");
+		
+		assertThat(p_0_1_r_X12.isSuccessful(), is(false));
+		assertThat(p_0_1_r_X12.canBacktrack(), is(true));
+		assertThat(p_0_1_r_X12.end().index(), is(1));
+		assertThat(p_0_1_r_X12.value(), is(nullValue()));
+		
+		assertThat(p_0_cut_1_r_X12.isSuccessful(), is(false));
+		assertThat(p_0_cut_1_r_X12.canBacktrack(), is(true));
+		assertThat(p_0_cut_1_r_X12.end().index(), is(1));
+		assertThat(p_0_cut_1_r_X12.value(), is(nullValue()));
+		
+		final ValResult<String> p_0_1_r_0X2 = p_0_1.parse("0X2");
+		final ValResult<String> p_0_cut_1_r_0X2 = p_0_cut_1.parse("0X2");
+		
+		assertThat(p_0_1_r_0X2.isSuccessful(), is(false));
+		assertThat(p_0_1_r_0X2.canBacktrack(), is(true));
+		assertThat(p_0_1_r_0X2.end().index(), is(2));
+		assertThat(p_0_1_r_0X2.value(), is(nullValue()));
+
+		assertThat(p_0_cut_1_r_0X2.isSuccessful(), is(false));
+		assertThat(p_0_cut_1_r_0X2.canBacktrack(), is(false));
+		assertThat(p_0_cut_1_r_0X2.end().index(), is(2));
+		assertThat(p_0_cut_1_r_0X2.value(), is(nullValue()));
+	}
+
+}
