ebef26a7de4061e31da95a56eb2d8d3d92644af3
Mizuky Fujitani <mizuki.fujitani@gmail.com>
2017-09-28 14:33:26 +0900

    keywordInの実装を改善しテストケースを追加

--- src/main/java/org/unclazz/parsec/KeywordInParser.java
+++ src/main/java/org/unclazz/parsec/KeywordInParser.java
@@ -4,22 +4,44 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeSet;
 
 import org.unclazz.parsec.data.Tuple;
 import org.unclazz.parsec.data.Tuple2;
 
 final class KeywordInParser extends Parser {
-	private final Parser _inner;
+	private final List<Tuple2<String,Integer>> _entries;
 	KeywordInParser(String[] keywords){
 		ParsecUtility.mustNotBeNull("keywords", keywords);
 		keywords = distinct(keywords);
 		if (keywords.length == 0) throw new IllegalArgumentException("keywords must not be empty.");
 		if (containsNullOrEmpty(keywords)) throw new IllegalArgumentException("keywords must not contain null or empty.");
-		_inner = compose(keywords);
+		_entries = keywordAndCommonPrefixLength(keywords);
+		_entries.add(Tuple.of(keywords[keywords.length - 1], -1));
+		
 	}
 	@Override
 	protected ResultCore doParse(Context ctx) throws IOException {
-		return _inner.parse(ctx);
+		final TextReader src = ctx.source();
+		Tuple2<String,Integer> entryCache = _entries.get(0);
+		
+		for (int i = 0, j = 0; i < entryCache.item1().length() && src.hasRemaining();) {
+			final char actual = (char) src.peek();
+			final char expected = entryCache.item1().charAt(i);
+			
+			if (actual == expected) {
+				src.read(); 
+				i++;
+			} else if (i <= entryCache.item2()) {
+				entryCache = _entries.get(++ j);
+			} else {
+				return failure("%s expected but %s found.",
+						ParsecUtility.charToString(expected),
+						ParsecUtility.charToString(actual));
+			}
+		}
+		
+		return success();
 	}
 	private static boolean containsNullOrEmpty(String[] ks) {
 		for (int i = 0; i < ks.length; i ++) {
@@ -30,47 +52,7 @@ final class KeywordInParser extends Parser {
 	}
 	private static String[] distinct(String[] ks) {
 		if (ks.length <= 1) return ks;
-		Arrays.sort(ks);
-		final String[] tmp = new String[ks.length];
-		int size = 0;
-		for (int i = size; i < ks.length; i ++) {
-			final String k = ks[i];
-			if (Arrays.binarySearch(tmp, k) != -1) {
-				ks[size ++] = k;
-			}
-		}
-		return Arrays.copyOf(tmp, size);
-	}
-	private static Parser compose(String[] ks) {
-        // キーワード数が1の場合、単にKeywordParserのインスタンスを返すだけ
-		if (ks.length == 1) return new KeywordParser(ks[0]);
-		
-        // 合成過程のパーサーを格納する一時変数
-		Parser tmp = null;
-
-        // キーワードのコレクションをそれ自身（ただし最初の要素はスキップ）とZIPして、
-        // {キーワード, 隣接するキーワードとの共通接頭辞の長さ}
-        // という2メンバーを持つ一時型インスタンスのシーケンスに変換
-		final List<Tuple2<String,Integer>> zip = keywordAndCommonPrefixLength(ks);
-		
-        // ZIPした結果に基づきループを行いパーサーを段階構築
-		for (final Tuple2<String,Integer> pair : zip) {
-            // キーワードの長さ と 共通接頭辞の長さ＋1 のいずれか小さい方をカット（Cut）の文字位置として採用
-            // ＊共通接頭辞の長さに＋1をすると、キーワードの長さをオーバーする可能性がある。
-            // これはKeywordParserのコンストラクタ引数の値として不正とみなされるので、
-            // キーワード自体の長さと比較を行い短い方を採用する。
-            final int cutIndex = Math.min(pair.item1().length(), pair.item2() + 1);
-			
-            // キーワードを単体で読み取るパーサーを作成
-            final Parser nextParser = new KeywordParser(pair.item1(), cutIndex);
-            
-            // 一時変数が空 ＝ 初回 は、作成したパーサーを単純にアサインする
-            // 一時変数が空でない ＝ 2回目以降 は、Orで連結して再アサインする
-            tmp = tmp == null ? nextParser : tmp.or(nextParser);
-		}
-        // 終わりに、キーワードのコレクションの末尾のものをOrで連結する
-        // ＊ZIPを行った時、末尾のキーワードは除外されてしまうため、ここで救済する
-        return tmp.or(new KeywordParser(ks[ks.length - 1]));
+		return new TreeSet<>(Arrays.asList(ks)).toArray(new String[0]);
 	}
 	private static int commonPrefixLength(String k0, String k1) {
 		final int loopCount = Math.min(k0.length(), k1.length());
---
+++ src/test/java/org/unclazz/parsec/KeywordInParserTest.java
@@ -0,0 +1,40 @@
+package org.unclazz.parsec;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+public class KeywordInParserTest {
+
+	@Test
+	public void testParseTextReader() {
+		final Parser p = Parsers.keywordIn("aaab", "aabb", "abbb");
+		
+		final Result r_aaaa = p.parse("aaaa");
+		final Result r_aaab = p.parse("aaab");
+		final Result r_aabb = p.parse("aabb");
+		final Result r_abbb = p.parse("abbb");
+		final Result r_bbbb = p.parse("bbbb");
+		
+		assertThat(r_aaaa.isSuccessful(), is(false));
+		assertThat(r_aaab.isSuccessful(), is(true));
+		assertThat(r_aabb.isSuccessful(), is(true));
+		assertThat(r_abbb.isSuccessful(), is(true));
+		assertThat(r_bbbb.isSuccessful(), is(false));
+		
+		final Result r_aaax = p.parse("aaax");
+		
+		assertThat(r_aaax.isSuccessful(), is(false));
+		assertThat(r_aaax.message(), is("'b'(98) expected but 'x'(120) found."));
+		assertThat(r_aaax.end().index(), is(3));
+
+		final Result r_aabx = p.parse("aaby");
+		assertThat(r_aabx.isSuccessful(), is(false));
+		assertThat(r_aabx.message(), is("'b'(98) expected but 'y'(121) found."));
+		
+		final Result r_abbx = p.parse("abbz");
+		assertThat(r_abbx.isSuccessful(), is(false));
+		assertThat(r_abbx.message(), is("'b'(98) expected but 'z'(122) found."));
+	}
+}
