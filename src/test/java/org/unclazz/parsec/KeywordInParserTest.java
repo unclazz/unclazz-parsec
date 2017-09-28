865873b2f72ad8a84ee4f580a41084be1900204d
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-29 01:33:43 +0900

    keywordInのテストケースを追加

--- src/test/java/org/unclazz/parsec/KeywordInParserTest.java
+++ src/test/java/org/unclazz/parsec/KeywordInParserTest.java
@@ -9,17 +9,19 @@ public class KeywordInParserTest {
 
 	@Test
 	public void testParseTextReader() {
-		final Parser p = Parsers.keywordIn("aaab", "aabb", "abbb");
+		final Parser p = Parsers.keywordIn("aaab", "aabb", "abbb", "aacc", "ccbb");
 		
 		final Result r_aaaa = p.parse("aaaa");
 		final Result r_aaab = p.parse("aaab");
 		final Result r_aabb = p.parse("aabb");
+		final Result r_aacc = p.parse("aacc");
 		final Result r_abbb = p.parse("abbb");
 		final Result r_bbbb = p.parse("bbbb");
 		
 		assertThat(r_aaaa.isSuccessful(), is(false));
 		assertThat(r_aaab.isSuccessful(), is(true));
 		assertThat(r_aabb.isSuccessful(), is(true));
+		assertThat(r_aacc.isSuccessful(), is(true));
 		assertThat(r_abbb.isSuccessful(), is(true));
 		assertThat(r_bbbb.isSuccessful(), is(false));
 		
@@ -36,5 +38,34 @@ public class KeywordInParserTest {
 		final Result r_abbx = p.parse("abbz");
 		assertThat(r_abbx.isSuccessful(), is(false));
 		assertThat(r_abbx.message(), is("'b'(98) expected but 'z'(122) found."));
+	}
+	@Test
+	public void testCostructor_abend() {
+		try {
+			final String[] a = null;
+			Parsers.keywordIn(a);
+		} catch (NullPointerException e) {
+			// OK
+		}
+		try {
+			Parsers.keywordIn("aaab", "aabb", "abbb", "aacc", "");
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+		try {
+			Parsers.keywordIn("aaab", "aabb", "abbb", "aacc", "aabb");
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+		try {
+			Parsers.keywordIn("aaab", null, "abbb", "aacc", "");
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
+		try {
+			Parsers.keywordIn();
+		} catch (IllegalArgumentException e) {
+			// OK
+		}
 	}
 }
