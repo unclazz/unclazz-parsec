9396f57c545e7b3f5ea16f16cf2d6304f5224bb7
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-10-01 19:22:09 +0900

    RepeatValParserのJavadocを加筆修正

--- src/main/java/org/unclazz/parsec/RepeatValParser.java
+++ src/main/java/org/unclazz/parsec/RepeatValParser.java
@@ -10,7 +10,7 @@ import java.util.function.Supplier;
 /**
  * パターンの繰返しをパースするパーサーです。
  * <p>このパーサーのインスタンスは{@link ValParser#rep()}およびその多重定義から得られます。</p>
- * @param <T> 
+ * @param <T> 読み取り結果型のリストの要素型
  */
 public final class RepeatValParser<T> extends ValParser<List<T>> {
 	private final ListParser<T> _inner;
@@ -30,34 +30,45 @@ public final class RepeatValParser<T> extends ValParser<List<T>> {
 	
 	/**
 	 * リダクションを行うパーサーを返します。
-	 * @param accumulator 
-	 * @return
+	 * <p>このオーバーロードはリダクションのシードとなる値を引数としてとりません。
+	 * このため元のパーサーの読み取り結果が空のリストとなる場合、リダクションを行うことができません。
+	 * リダクションが行えなかった場合、このメソッドが返すパーサーの読み取り結果は{@link Optional#empty()}が返す値となります。</p>
+	 * @param accumulator アキュムレート（累積）のオペレーションを行う関数 
+	 * @return リダクションされた結果を読み取り結果型とするパーサー
 	 */
 	public ValParser<Optional<T>> reduce(BiFunction<T, T, T> accumulator){
 		return _inner.reReduce(accumulator);
 	}
 	/**
 	 * リダクションを行うパーサーを返します。
-	 * @param seed
-	 * @param accumulator
-	 * @return
+	 * <p>このオーバーロードはリダクションのシードとなる値を生成する関数を引数にとります。
+	 * 元のパーサーの読み取り結果が空のリストとなる場合、このシードの値がそのまま結果となります。</p>
+	 * @param seed シードを生成する関数
+	 * @param accumulator アキュムレート（累積）のオペレーションを行う関数 
+	 * @return リダクションされた結果を読み取り結果型とするパーサー
+	 * @param <U> アキュムレートの過程・結果の値の型
 	 */
 	public<U> ValParser<U> reduce(Supplier<U> seed, BiFunction<U, T, U> accumulator){
 		return _inner.reReduce(seed, accumulator);
 	}
 	/**
 	 * リダクションを行うパーサーを返します。
-	 * @param seed
-	 * @param accumulator
-	 * @param resultSelector
-	 * @return
+	 * <p>このオーバーロードはリダクションのシードとなる値を生成する関数を引数にとります。
+	 * 元のパーサーの読み取り結果が空のリストとなる場合、このシードの値がそのままアキュムレートの結果となります。
+	 * このオーバーロードはまたアキュムレート結果を変換して最終結果を得るための関数を引数に取ります。</p>
+	 * @param seed シードを生成する関数
+	 * @param accumulator アキュムレート（累積）のオペレーションを行う関数 
+	 * @param resultSelector アキュムレートの結果を変換して最終結果を得るための関数
+	 * @return リダクションされた結果を読み取り結果型とするパーサー
+	 * @param <U> アキュムレートの過程・結果の値の型
+	 * @param <R> アキュムレート結果を変換した最終結果の値の型
 	 */
 	public<U,R> ValParser<R> reduce(Supplier<U> seed, BiFunction<U, T, U> accumulator, Function<U, R> resultSelector){
 		return _inner.reReduce(seed, accumulator, resultSelector);
 	}
 	/**
 	 * 繰返しの回数をカウントするパーサーを返します。
-	 * @return
+	 * @return 繰返しの回数をカウントするパーサー
 	 */
 	public ValParser<Integer> count() {
 		return _inner.reReduce(() -> 0, (a, b) -> a + 1);
